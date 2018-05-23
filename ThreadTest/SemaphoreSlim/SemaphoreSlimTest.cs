654095e80210422786da770b73ec6fca244f9bb5
DESKTOP-TPVOJVC\wangxmPC <wangxm@easyretailpro.com>
2018-05-23 17:27:59 +0800

    no message

---
+++ ThreadTest/SemaphoreSlim/SemaphoreSlimTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest.SemaphoreSlimA
+{
+    public class SemaphoreSlimTest
+    {
+        static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(2);
+
+        static void Run()
+        {
+            // currentVal = Interlocked.CompareExchange(ref target, desiredVal, startVal);
+            // 将target的值和startVal的值比较，相等则用desiredVal替换target，否则不操作
+            // 不管替换还是不替换返回的都是原来保存在target的值。
+            int x = 1;
+            var y = Interlocked.CompareExchange(ref x, 2, 1);
+
+            for (int i = 0; i <= 6; i++)
+            {
+                string threadName = "Thread" + i;
+                int secondsToWait = 2 + 2 * i;
+                var t = new Thread(() => ShowMessage(threadName, secondsToWait));
+                t.Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ShowMessage(string name, int second)
+        {
+            Console.WriteLine("{0} watis to showmessage", name);
+            semaphoreSlim.Wait();
+            Console.WriteLine("{0} was showmessage", name);
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            Console.WriteLine("{0} is completed", name);
+            semaphoreSlim.Release();
+        }
+
+    }
+}
---
+++ ThreadTest/Wait/WaitPulseTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest.Wait
+{
+    public class LockMe
+    {
+    }
+
+    class WaitPulse1
+    {
+        private int result = 0;
+        private LockMe lM;
+
+        public WaitPulse1()
+        {
+        }
+
+        public WaitPulse1(LockMe l)
+        {
+            this.lM = l;
+        }
+
+        public void CriticalSection()
+        {
+            Monitor.Enter(this.lM);
+            ////Enter the Critical Section
+            Console.WriteLine("WaitPulse1: Entered Thread " + Thread.CurrentThread.GetHashCode());
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Monitor.Wait(this.lM);
+                Console.WriteLine("WaitPulse1: WokeUp");
+                Console.WriteLine("WaitPulse1: Result = " + result++ + " ThreadID " + Thread.CurrentThread.GetHashCode());
+                Monitor.Pulse(this.lM);
+            }
+            Console.WriteLine("WaitPulse1: Exiting Thread " + Thread.CurrentThread.GetHashCode());
+
+            //Exit the Critical Section
+            Monitor.Exit(this.lM);
+        }
+    }
+
+    class WaitPulse2
+    {
+        private int result = 0;
+        private LockMe lM;
+
+        public WaitPulse2()
+        {
+        }
+
+        public WaitPulse2(LockMe l)
+        {
+            this.lM = l;
+        }
+
+        public void CriticalSection()
+        {
+            Monitor.Enter(this.lM);
+            //Enter the Critical Section
+            Console.WriteLine("WaitPulse2: Entered Thread "
+                + Thread.CurrentThread.GetHashCode());
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Monitor.Pulse(this.lM);
+                Console.WriteLine("WaitPulse2: Result = "
+                    + result++
+                    + " ThreadID "
+                    + Thread.CurrentThread.GetHashCode());
+                Monitor.Wait(this.lM);
+                Console.WriteLine("WaitPulse2: WokeUp");
+            }
+            Console.WriteLine("WaitPulse2: Exiting Thread "
+                + Thread.CurrentThread.GetHashCode());
+
+            //Exit the Critical Section
+            Monitor.Exit(this.lM);
+        }
+    }
+
+    public class WaitPulseTest
+    {
+        public static void Run()
+        {
+            LockMe l = new LockMe();
+
+            WaitPulse1 e1 = new WaitPulse1(l);
+            WaitPulse2 e2 = new WaitPulse2(l);
+
+            Thread t1 = new Thread(new ThreadStart(e1.CriticalSection));
+            t1.Start();
+
+            Thread t2 = new Thread(new ThreadStart(e2.CriticalSection));
+            t2.Start();
+
+            //Wait till the user enters something
+            Console.ReadLine();
+        }
+    }
+
+}
